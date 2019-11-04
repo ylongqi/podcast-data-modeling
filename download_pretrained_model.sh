186771ebe4e4e15114f96ad8cc48d1416cdc1dc5
Longqi Yang <ylongqi@gmail.com>
2019-11-04 09:02:40 -0800

    Update download links

--- download_pretrained_model.sh
+++ download_pretrained_model.sh
@@ -2,6 +2,6 @@
 
 mkdir pretrained_model;
 
-wget https://www.dropbox.com/s/zn8x1p4e7knq9tb/alpr.data-00000-of-00001?dl=1 -O pretrained_model/alpr.data-00000-of-00001;
-wget https://www.dropbox.com/s/di6j8dd9hnr5vyn/alpr.index?dl=1 -O pretrained_model/alpr.index;
-wget https://www.dropbox.com/s/ll3uw4taovinvv4/alpr.meta?dl=1 -O pretrained_model/alpr.meta;
+wget https://storage.googleapis.com/cornell-tech-sdl-podcast-dataset/alpr.data-00000-of-00001 -O pretrained_model/alpr.data-00000-of-00001;
+wget https://storage.googleapis.com/cornell-tech-sdl-podcast-dataset/alpr.index -O pretrained_model/alpr.index;
+wget https://storage.googleapis.com/cornell-tech-sdl-podcast-dataset/alpr.meta -O pretrained_model/alpr.meta;
