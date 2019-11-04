8fbe6416c707990b7f1ba2c30dbcdb089657c884
Longqi Yang <ylongqi@gmail.com>
2019-11-04 08:59:38 -0800

    Update download link

--- download_transcriptions_popularity.sh
+++ download_transcriptions_popularity.sh
@@ -2,5 +2,5 @@
 
 mkdir -p data/popularity_prediction_transcriptions;
 
-wget https://www.dropbox.com/s/32qaoz6vqkdgikr/popularity_prediction_transcriptions.tar.gz?dl=1 -O data/popularity_prediction_transcriptions.tar.gz;
+wget https://storage.googleapis.com/cornell-tech-sdl-podcast-dataset/popularity_prediction_transcriptions.tar.gz -O data/popularity_prediction_transcriptions.tar.gz;
 tar -xzf data/popularity_prediction_transcriptions.tar.gz -C data/popularity_prediction_transcriptions --verbose;
