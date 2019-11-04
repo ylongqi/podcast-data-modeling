9e8509dbdcdc0f7b09f9b2c9683e228fc869e80c
Longqi Yang <ylongqi@gmail.com>
2019-11-04 08:57:31 -0800

    Update download link

--- download_spectrograms_attributes.sh
+++ download_spectrograms_attributes.sh
@@ -2,5 +2,5 @@
 
 mkdir -p data/attributes_prediction_spectrograms;
 
-wget https://www.dropbox.com/s/2v72tiwqd5h3ual/attributes_prediction_spectrograms.tar.gz?dl=1 -O data/attributes_prediction_spectrograms.tar.gz;
+wget https://storage.googleapis.com/cornell-tech-sdl-podcast-dataset/attributes_prediction_spectrograms.tar.gz -O data/attributes_prediction_spectrograms.tar.gz;
 tar -xzf data/attributes_prediction_spectrograms.tar.gz -C data/attributes_prediction_spectrograms --verbose;
