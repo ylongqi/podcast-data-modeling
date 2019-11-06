d43bbfd90028aa789f7bd99c8c9a787ec45bb6a6
Longqi Yang <ylongqi@gmail.com>
2019-11-06 10:29:51 -0800

    Update download link

--- download_audio_popularity.sh
+++ download_audio_popularity.sh
@@ -2,5 +2,5 @@
 
 mkdir -p data/popularity_prediction_raw_audio;
 
-wget https://www.dropbox.com/s/jytsd3zx8luc5bm/popularity_prediction_raw_audio.tar.gz?dl=1 -O data/popularity_prediction_raw_audio.tar.gz;
+wget https://storage.googleapis.com/cornell-tech-sdl-podcast-dataset/popularity_prediction_raw_audio.tar.gz -O data/popularity_prediction_raw_audio.tar.gz;
 tar -xzf data/popularity_prediction_raw_audio.tar.gz -C data/popularity_prediction_raw_audio --verbose;
